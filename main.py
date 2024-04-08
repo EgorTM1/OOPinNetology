924e68099fa70944498154cf16132ba33862cba9
Egor <tolmacevegor46@gmail.com>
2024-04-08 20:58:00 +0300

    Add coments

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 class Student:
+    """
+    Этот класс позволяет работы со студентами, их оценками и курсами, которые они изучают, 
+    а также обеспечивает функционал для сравнения студентов по их успеваемости. """
+
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
@@ -29,12 +33,23 @@ class Student:
         else: 
             return 'Можно сравнивать только студентов!'
 class Mentor:
+    """Инициализатор _init_ для определения атрибутов класса Mentor
+        Содержит атрибуты:
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        """
+    
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
 class Lecturer(Mentor):
+    """Этот класс предоставляет возможность работать с 
+        экземплярами студентов и преподавателями, их оценками
+        и сравнивать их между собой по успеваемости ведения лекций."""
+    
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
@@ -54,6 +69,9 @@ class Lecturer(Mentor):
             print('Можно сравнивать только лекторов!')
 
 class Reviewer(Mentor):
+    '''Этот класс обеспечивает функционал для рецензирования домашних заданий студентов
+       и добавляет оценки в их список оценок для соответствующих курсов.'''
+    
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -66,6 +84,7 @@ class Reviewer(Mentor):
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}'
 
+# Создаем экземпляры класса Student
 student1 = Student('Egor', 'Sidorov')
 student1.courses_in_progress += ['Python', 'Git']
 student1.finished_courses += ['Введение в программирование']
@@ -74,14 +93,14 @@ student2 = Student('Pavel', 'Ivanov')
 student2.courses_in_progress += ['Python', 'Git']
 student2.finished_courses += ['Введение в программирование']
 
-
+# Создаем экземпляры класса Lecturer
 lecturer1 = Lecturer('Oleg', 'Petrov')
 lecturer1.courses_attached += ['Python']
 
 lecturer2 = Lecturer('Igor', 'Chernov')
 lecturer2.courses_attached += ['Git']
 
-
+# Создаем экземпляры класса Reviewer
 reviewer1 = Reviewer('Nikolay', 'Vivaldi')
 reviewer1.courses_attached += ['Python', 'Git']
 
@@ -89,7 +108,7 @@ reviewer2 = Reviewer('Elena', 'Nikitina')
 reviewer2.courses_attached += ['Python', 'Git']
 
 
-
+# Выставляем оценки за лекции студентам
 student1.rate_hw(lecturer1, 'Python', 10)
 student1.rate_hw(lecturer1, 'Python', 9)
 
@@ -106,7 +125,7 @@ student2.rate_hw(lecturer2, 'Git', 8)
 student2.rate_hw(lecturer2, 'Git', 9)
 
 
-
+# Выставляем оценки за домашние задания
 reviewer1.rate_hw(student1, 'Python', 8)
 reviewer1.rate_hw(student1, 'Python', 9)
 
@@ -135,11 +154,13 @@ print(f'Результат сравнения лекторов по средни
       f'{lecturer1.name} {lecturer1.surname} < {lecturer2.name} {lecturer2.surname} = {lecturer1 > lecturer2}\n')
 
 
+# Создаем списки студентов и лекторов
 all_students = [student1, student2]
 
 all_lecturers = [lecturer1, lecturer2]
 
 
+# Функция для вычисления средней оценки студентов
 def average_hw_grade_of_students(students, course):
     summi = 0
     count = 0
@@ -154,6 +175,7 @@ def average_hw_grade_of_students(students, course):
 print(average_hw_grade_of_students(all_students, 'Python'))
 print(average_hw_grade_of_students(all_students, 'Git'))
 
+# Функция для вычисления средней оценки лекторов
 def average_hw_grade_of_lecturers(lecturers, course):
     summi = 0
     count = 0
